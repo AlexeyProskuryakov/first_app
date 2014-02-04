2d39e9ce0ba1219c95f1001e6ed91fecab035a5b
Alexey Proskuryakov <alexey.proskuryakov@gmail.com>
2014-02-05 02:48:10 +0400

    static pages end

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,8 +3,35 @@ require 'spec_helper'
 describe "Static pages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
-      visit 'static_pages/home'
+      visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    it "should have the right title" do
+    	visit '/static_pages/home'
+    	expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+    end
   end
+
+  describe "Help page" do
+  	it "should have content 'Help'" do
+  		visit '/static_pages/help'
+  		expect(page).to have_content('Help')
+  	end
+  	it "should have the right title" do
+    	visit '/static_pages/help'
+    	expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+    end
+  end
+
+  describe "About page" do
+  	it "should have content 'About Us'" do
+  		visit '/static_pages/about_us'
+  		expect(page).to have_content('About Us')
+  	end
+  	it "should have the right title" do
+    	visit '/static_pages/about_us'
+    	expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+    end
+  end
+
 end
