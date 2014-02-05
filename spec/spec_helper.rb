0cd1f5c7716838de4b5eeedba444b5810b838e5f
Alexey Proskuryakov <alexey.proskuryakov@gmail.com>
2014-02-05 14:32:48 +0400

    environment ith spork and guard

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ Spork.prefork do
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path('../../config/environment',__FILE__)
   require 'rspec/rails'
-  require 'autorun'
+  require 'rspec/autorun'
 
   Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f  }
 
