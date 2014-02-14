a9e7a73c10e16641e447d7e2ebb7fe023856c3f4
Alexey Proskuryakov <alexey.proskuryakov@gmail.com>
2014-02-14 04:05:10 +0400

    realise some exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,6 +21,12 @@ describe "UserPages" do
 			it "should not create user" do
 				expect {click_button submit}.not_to change(User,:count)
 			end
+			describe "after submission" do
+				before {click_button submit}
+				it {should have_title('Sign up')}
+				it {should have_content('error')}
+			end
+			
 		end
 
 		describe "with valid information" do 
@@ -32,6 +38,12 @@ describe "UserPages" do
 			end
 			it "should create a user" do
 				expect {click_button submit}.to change(User,:count).by(1)
+			end
+			describe "after saving user" do
+				before {click_button submit}
+				let(:user) {User.find_by(email: 'a@a.ru')}
+				it {should have_title(user.name)}
+				it {should have_selector('div.alert.alert-success', text: 'Welcome')}
 			end
 		end
 	end
